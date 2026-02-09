26536251acd2f0e7926f658570a798731038e27d
Daniel Serrano <dserranoc@indra.es>
2026-02-09 10:40:43 +0100

    Category Creation and API Docker connection

---
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
