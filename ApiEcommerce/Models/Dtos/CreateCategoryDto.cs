548d2076d6be15187c46d23ab658d402dff1ae51
Daniel Serrano <dserranoc@indra.es>
2026-02-10 10:07:17 +0100

    Automapper Configuration basic Data

--- ApiEcommerce/Models/Dtos/CreateCategoryDto.cs
+++ ApiEcommerce/Models/Dtos/CreateCategoryDto.cs
@@ -8,5 +8,5 @@ public class CreateCategoryDto
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     [MaxLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
     [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
